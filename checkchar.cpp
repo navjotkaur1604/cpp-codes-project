86cdeb650874b6eb32a080d80cd668cf38341e6e
Navjot Kaur <navjotkaur4250@gmail.com>
2025-07-09 14:39:35 +0530

    adding files

---
+++ checkchar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch; 
+if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        cout << ch << " is an Alphabet." << endl;
+    }
+    else if (ch >= '0' && ch <= '9') {
+        cout << ch << " is a Digit." << endl;
+    }
+    else {
+        cout << ch << " is a Special Character." << endl;
+    }
+}
---
+++ vowel.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            cout << ch << " is a Vowel." << endl;
+        } else {
+            cout << ch << " is a Consonant." << endl;
+        }
+    } else {
+        cout << "Not an alphabet." << endl;
+    }
+
+    return 0;
+}
